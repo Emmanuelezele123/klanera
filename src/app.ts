5ceb891fbd320857be2565c2f1111771d17a194d
Emmanuel Ezele <emmaezele@yahoo.com>
2023-05-04 19:35:03 +0100

    add stuff

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,8 @@ const cookieParser = require('cookie-parser');
 import connectDB from "../config/db";
 const authRouter = require("../routes/auth")
 const accountRouter = require("../routes/account")
-const swaggerUI = require("swagger-ui-express");
-const swaggerDoc =  require("swagger-jsdoc");
 const User  =  require("../models/user");
 const app:Application = express()
-app.use(cors())      
 app.use(express.json()) 
 app.use(cookieParser());
 app.use(
