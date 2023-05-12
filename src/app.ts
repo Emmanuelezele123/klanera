3151d813bdd62309d2ee55e9084eb39689a13a42
Oluwatimilehin Alabi <alabialvin@gmail.com>
2023-05-12 18:02:56 +0100

    added /ap1/v1 to routes

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,28 @@
-
 import express, { Application, Request, Response, NextFunction } from "express";
-import cors from 'cors';
-const cookieParser = require('cookie-parser');
+import cors from "cors";
+const cookieParser = require("cookie-parser");
 import connectDB from "../config/db";
-const authRouter = require("../routes/auth")
-const accountRouter = require("../routes/account")
-const User  =  require("../models/user");
-const app:Application = express()
-app.use(express.json()) 
+const authRouter = require("../routes/auth");
+const accountRouter = require("../routes/account");
+const User = require("../models/user");
+const app: Application = express();
+app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "https://klanera.vercel.app",
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
-  }),
+	cors({
+		credentials: true,
+		origin: [
+			"https://klanera.vercel.app",
+			"http://localhost:3000",
+			"http://localhost:3001",
+		],
+	})
 );
-app.use("/user",authRouter)
-app.use("/account",accountRouter)
+app.use("/api/v1/user", authRouter);
+app.use("/api/v1/account", accountRouter);
 // Root route of express app
 app.get("/", (req, res) => {
-  res.send("klanera api");
-}); 
- 
+	res.send("klanera api");
+});
 
-connectDB(app)
+connectDB(app);
