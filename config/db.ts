81d2ca71df411bd9b40442e947a43664745e47b7
Oluwatimilehin Alabi <alabialvin@gmail.com>
2023-06-11 18:18:43 +0100

    add: expire time to otp document

--- config/db.ts
+++ config/db.ts
@@ -1,27 +1,42 @@
-import mongoose, { ConnectOptions } from 'mongoose';
-import { config } from 'dotenv';
-import { Application } from 'express';
+import mongoose, { ConnectOptions } from "mongoose";
+import { config } from "dotenv";
+import { Application } from "express";
+import { Socket } from "socket.io";
+const socket = require("socket.io");
 
 config();
 
 const { MONGO_URI }: NodeJS.ProcessEnv = process.env;
 
-const connectDB = (app:Application) => {
-  const options: any = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
-  mongoose
-    .connect(MONGO_URI!, options)
-    .then(() => {
-      app.listen(process.env.PORT || 3000, () => {
-        console.log('klanera APP is running on');
-      });
-    })
-    .catch((err: Error) => {
-      console.log(err.message);
-      console.error(err);
-    });
+const connectDB = (app: Application) => {
+	const options: any = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	mongoose
+		.connect(MONGO_URI!, options)
+		.then(() => {
+			const server = app.listen(process.env.PORT || 3000, () => {
+				console.log("klanera APP is running on");
+			});
+
+			const io = socket(server, {
+				cors: {
+					origin: "http://localhost:5000",
+					credentials: true,
+				},
+			});
+
+			io.on("connection", () => {
+				console.log("A user connected");
+			});
+
+			require("../config/socket")(io);
+		})
+		.catch((err: Error) => {
+			console.log(err.message);
+			console.error(err);
+		});
 };
 
 export default connectDB;
--- models/otp.js
+++ models/otp.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const otpSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "user",
-    },
-    otpText: {
-        type: String,
-        required: true,
-    }
+	userId: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		ref: "user",
+	},
+	otpText: {
+		type: String,
+		required: true,
+	},
+	createdAt: { type: Date, expires: "10m", default: Date.now }, // TTL index for 10 minutes
 });
 
 module.exports = mongoose.model("Otp", otpSchema);
